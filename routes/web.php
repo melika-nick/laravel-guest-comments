b9619a3f8184266e73bb1dda75d80d5ce03f2f48
melika-nick <nikmelika8@gmail.com>
2025-07-27 12:47:06 +0330

    set routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\PostController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::prefix('admin')->group(function () {
+    Route::resource('posts', PostController::class);
+});
+
