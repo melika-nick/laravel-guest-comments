563c1dbb8606342970626c9b4f2a84d9943dbd47
melika-nick <nikmelika8@gmail.com>
2025-07-27 12:55:53 +0330

    create ui controller to show posts

---
+++ app/Http/Controllers/UI/PostUIController.php
@@ -0,0 +1,43 @@
+<?php
+namespace App\Http\Controllers\UI;
+
+use App\Http\Controllers\Controller;
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostUIController extends Controller
+{
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
+    }
+
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    public function store(Request $request)
+    {
+        Post::create($request->only('title', 'content'));
+        return redirect()->route('posts.index');
+    }
+
+    public function edit(Post $post)
+    {
+        return view('posts.edit', compact('post'));
+    }
+
+    public function update(Request $request, Post $post)
+    {
+        $post->update($request->only('title', 'content'));
+        return redirect()->route('posts.index');
+    }
+
+    public function destroy(Post $post)
+    {
+        $post->delete();
+        return redirect()->route('posts.index');
+    }
+}
