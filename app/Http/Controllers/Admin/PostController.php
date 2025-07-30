a7f9c70e5a85d9a9ea88184c26236d2aaf21e809
melika-nick <nikmelika8@gmail.com>
2025-07-30 10:03:34 +0330

    redesign files and directories

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -9,41 +9,20 @@ use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
     public function index()
     {
         $posts = Post::all();
         $comments = Comment::all();
-        return view('posts.index', compact('posts', 'comments'));
+        return view('admin.posts.index', compact('posts', 'comments'));
+    }
+    public function create()
+    {
+        return view('posts.create');
     }
 
     public function store(Request $request)
     {
-        return Post::create($request->only('title', 'content'));
-    }
-
-    /**
-     * Display the specified resource.
-     */
-    public function show(Post $post)
-    {
-        return $post;
-    }
-
-    public function update(Request $request, Post $post)
-    {
-        $post->update($request->only('title', 'content'));
-        return $post;
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     */
-    public function destroy(Post $post)
-    {
-        $post->delete();
-        return response()->noContent();
+        Post::create($request->only('title', 'content'));
+        return redirect()->route('admin.posts.index');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -9,14 +9,14 @@ use App\Http\Controllers\UI\PostUIController;
 //use App\Http\Controllers\CommentController;
 
 Route::controller(PostUIController::class)->prefix('admin/posts')->name('posts.')->group(function () {
-    Route::get('/', 'indexAdmin')->name('index');
     Route::get('/create', 'create')->name('create');
     Route::post('/', 'store')->name('store');
     Route::get('/{post}/edit', 'edit')->name('edit');
     Route::put('/{post}', 'update')->name('update');
     Route::delete('/{post}', 'destroy')->name('destroy');
 });
-Route::get('admin/posts', [AdminPostController::class, 'index'])->name('admin.posts');
+Route::get('admin/posts', [AdminPostController::class, 'index'])->name('posts');
+Route::get('admin/posts/create', [AdminPostController::class, 'create'])->name('posts.create');
 
 Route::get('user/index', [UserPostController::class, 'index']);
 Route::post('posts/{post}/comments', [UserCommentController::class, 'store']);
